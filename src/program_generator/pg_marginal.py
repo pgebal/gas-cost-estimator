a795cae2068a7099d252f71718a23f73ebacc16b
Pawel Gebal <pawel.gebal@imapp.pl>
2024-07-15 11:26:19 +0200

    Revert "change program"
    This reverts commit c383889bb8f1ee6640d2e687641571cfd5912e25.

--- src/program_generator/pg_marginal.py
+++ src/program_generator/pg_marginal.py
@@ -95,7 +95,6 @@ class ProgramGenerator(object):
 
   def _generate_single_program(self, operation, op_count):
     # for compatibility with the generate_single_marginal function
-    print(arity(operation))
     single_op_pushes = ["60ff"] * arity(operation)
 
     return Program(generate_single_marginal(single_op_pushes, operation, op_count), operation['Mnemonic'], op_count)
